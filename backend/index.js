631d20eeb378ddca31879343a4141c69072ab4b1
Petteri Parviainen <daolamis@gmail.com>
2021-09-02 10:58:10 +0300

    implemented exercises 8.13 and 8.14

--- backend/index.js
+++ backend/index.js
@@ -1,89 +1,18 @@
+require('dotenv').config()
 const { ApolloServer, gql } = require('apollo-server')
 const { v1: uuid } = require('uuid')
+const mongoose = require('mongoose')
+const Book = require('./models/book')
+const Author = require('./models/author')
+const { ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginLandingPageDisabled } = require('apollo-server-core')
 
-const { ApolloServerPluginLandingPageGraphQLPlayground, ApolloServerPluginLandingPageDisabled } = require('apollo-server-core');
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  {
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  {
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
+const MONGODB_URI = process.env.MONGODB_URI
+console.log('connecting to', MONGODB_URI)
 
-/*
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
-*/
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(e => console.log('Failed to connect MongoDB', e.message))
 
 const typeDefs = gql`
   type Author {
@@ -97,7 +26,7 @@ const typeDefs = gql`
     id: ID!
     title: String!
     published: Int!
-    author: String!
+    author: Author!
     genres: [String!]!
   }
     
@@ -122,45 +51,42 @@ const typeDefs = gql`
 
 const resolvers = {
   Mutation: {
-    addBook: (root, args) => {
-      if (!authors.find(a => a.name.toUpperCase() === args.author.toUpperCase())) {
-        authors.push({ id: uuid(), name: args.author })
+    addBook: async (root, args) => {
+      let author = await Author.findOne({ name: args.author })
+      if (!author) {
+        author = new Author({ name: args.author })
+        author = await author.save()
       }
-      const newBook = {
-        id: uuid(),
-        title: args.title,
-        author: args.author,
-        published: args.published,
-        genres: args.genres,
-      }
-      books.push(newBook)
-      return newBook
+      const book = new Book({ ...args, author: author })
+      return book.save()
     },
-    editAuthor: (root, args) => {
-      const author = authors.find(a => a.name === args.name);
+    editAuthor: async (root, args) => {
+      const author = await Author.findOne({ 'name': args.name })
       if (author) {
         author.born = args.setToBorn
+        return author.save()
       }
-      return author
+      return null
     }
   },
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
+    bookCount: () => Books.collection.countDocuments(),
+    authorCount: () => Author.collection.countDocuments(),
     allBooks: (root, args) => {
-      return args.author || args.genre
-        ? books.filter(b => {
-          const authorFilter = args.author ? b.author === args.author : true
-          const genreFilter = args.genre ? b.genres.includes(args.genre) : true
-          // console.log('authorFilter', authorFilter, 'genreFilter', genreFilter)
-          return authorFilter && genreFilter;
-        })
-        : books
+      if (args.genre) {
+        return Book.find({ 'genres': { $in: [args.genre] } })
+          .populate('author')
+      }
+      return Book.find({}).populate('author')
     },
-    allAuthors: () => authors,
+    allAuthors: () => Author.find({}),
   },
   Author: {
-    bookCount: (root) => books.filter(b => b.author === root.name).length
+    bookCount: async (root) => {
+      const books = await Book.find({ author: root.id })
+      console.log('BOOKS', books)
+      return books.length
+    }
   }
 }
 
@@ -168,10 +94,8 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   plugins: [
-    ApolloServerPluginLandingPageGraphQLPlayground({
-      // options
-    })
-    , ApolloServerPluginLandingPageDisabled()
+    ApolloServerPluginLandingPageGraphQLPlayground({}),
+    ApolloServerPluginLandingPageDisabled()
   ]
 })
 
